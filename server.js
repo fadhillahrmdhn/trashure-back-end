ea0e4f2aac93befecb0d392bd40d15a61662878b
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-12-01 07:49:54 +0700

    mengembalikan nilai variabel lama

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ sequelize
   .then(() => {
     console.log('database berhasil disambungkan');
     // starting server and
-    const PORT = process.env.PORT || 6845;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode port ${PORT}`));
   })
   .catch(() => {
