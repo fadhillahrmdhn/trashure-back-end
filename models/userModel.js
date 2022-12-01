701cfa878ae4d1e177511ee554a5eb4e3e6a3f79
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-12-01 21:15:45 +0700

    mengubah autoincrement

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,11 @@
-const { DataTypes, UUIDV4 } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/database');
 
 const User = sequelize.define('user', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
-    defaultValue: UUIDV4,
+    autoIncrement: true,
     allowNull: false,
   },
   name: {
