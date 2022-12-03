9f0d7c0614fc9ad939e92dc1e44cf4f453b582d6
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-12-03 16:31:59 +0700

    update pw

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,18 +88,18 @@ module.exports = {
 
       if (!req.body.password) {
         await user.update(getHashedPassword(req.body.password));
-        await user.update(req.body);
         res.status(200).json({
           succes: true,
           message: 'data user berhasil diperbaharui',
         });
+      } else {
+        await user.update(req.body);
+        res.status(200).json({
+          succes: true,
+          message: 'password berhasil diperbaharui',
+          data: user,
+        });
       }
-      await user.update(getHashedPassword(req.body.password));
-      res.status(200).json({
-        succes: true,
-        message: 'password berhasil diperbaharui',
-        data: user,
-      });
     } catch (err) {
       res.status(400).json({
         succes: false,
