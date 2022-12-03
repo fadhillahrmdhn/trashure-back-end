4f3c7355e31638bc020e39fa2193111cae0a6a9f
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-12-03 14:55:00 +0700

    update pw pada userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,21 +85,13 @@ module.exports = {
           message: 'user tidak ditemukan',
         });
       }
-
-      const hashedPassword = getHashedPassword(req.body.password);
-      const userUpdate = await User.update({
-        name: req.body.name,
-        password: hashedPassword,
-        email: req.body.email,
-        number: req.body.number,
-        nik: req.body.nik,
-        alamat: req.body.alamat,
-      });
+      getHashedPassword(req.body.password);
+      await user.update(req.body);
 
       res.status(200).json({
         succes: true,
         message: 'data user berhasil diperbaharui',
-        data: userUpdate,
+        data: user,
       });
     } catch (err) {
       res.status(400).json({
