977f4e2c8d5128615e5999fd321ece4b2a179a5f
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-12-04 20:18:49 +0700

    menambahkan upload image di anorganik

--- routes/anorganikRoutes.js
+++ routes/anorganikRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
 const {
@@ -9,7 +10,18 @@ const {
   deleteAnorganik,
 } = require('../controllers/anorganikController');
 
-router.route('/').get(getAllAnorganiks).post(createAnorganik);
+// upload foto
+const storage = multer.diskStorage({
+  destination(reg, file, cb) {
+    cb(null, 'images');
+  },
+  filename(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage }).single('image');
+
+router.route('/').get(getAllAnorganiks).post(upload, createAnorganik);
 
 router.route('/:id').get(getAnorganik).put(editAnorganik).delete(deleteAnorganik);
 
